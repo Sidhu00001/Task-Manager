c2f9ce04907780dc629ffe4a5846f11db2244fff
Siddharth Chauhan <siddharthchauhan847@gmail.com>
2025-06-25 16:03:05 +0530

    adding register and login route

--- Models/user.js
+++ Models/user.js
@@ -1,11 +1,22 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
 const schema=mongoose.Schema;
 const userSchema= new schema({
     name:{type:String ,required :true},
     email:{type:String ,required :true,unique:true},
     password:{type:String ,required :true},
-})
-const user=mongoose.model('user',userSchema);
+});
+userSchema.pre('save',async function(next){
+   const user=this;
+   if(user.isModified('password')){
+    user.password=await bcrypt.hash(user.password,5);  
+   }
+    next();
+});
+userSchema.methods.comparePassword=async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+const User=mongoose.model('User',userSchema);
 module.exports={
-    user
+    User
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const {user}= require('../Models/user');
+const {User}= require('../Models/user');
+const bcrypt=require('bcrypt');
+const jsonwebtoken=require('jsonwebtoken');
+require('dotenv').config();
 
-router.get('/',(req,res)=>{
-    res.send('user routes are working');
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).send({ message: 'User created successfully' });
+    } catch (err) {
+        res.status(400).send({ error: err.message }); // <-- fix here
+    }
+});
+
+router.post('/login',async (req,res)=>{
+ try{
+    const {email,password}=req.body;
+    const user=await User.findOne({email});
+    if(!user){
+        throw new Error('User not found');
+    }
+    const isMatch=await bcrypt.compare(password,user.password); 
+    if(!isMatch){
+        throw new Error('Invalid credentials');
+    }
+    const token=jsonwebtoken.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'1h'});
+res.status(200).send({message:'Login successful',token});
+}
+catch(err){
+    res.status(400).send({error: err.message});
+}
+
 });
 module.exports=router;
